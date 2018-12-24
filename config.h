91ebcf611c27e3ee7572263c0c63afb7e0d4e226
hisao <37037970+sarishinohara@users.noreply.github.com>
2018-12-24 19:27:17 +0100

    hmm

--- config.h
+++ config.h
@@ -9,16 +9,16 @@ enum {
 /* bar font:
  * (see fonts-conf(5) subsection "FONT NAMES" for valid values)
  */
-static const char * const BAR_FONT = "bebas neue:size=8";
+static const char * const BAR_FONT = "nu:size=8";
 
 /* colors:
  * (see X(7) section "COLOR NAMES" for valid values)
  */
-static const char * const WIN_BG_COLOR = "#ffffff";
-static const char * const WIN_FS_COLOR = "#e47d73";
-static const char * const SEL_COLOR    = "#e47d73";
-static const char * const BAR_BG_COLOR = "#ffffff";
-static const char * const BAR_FG_COLOR = "#e47d73";
+static const char * const WIN_BG_COLOR = "#071d22";
+static const char * const WIN_FS_COLOR = "#37bf8d";
+static const char * const SEL_COLOR    = "#37bf8d";
+static const char * const BAR_BG_COLOR = "#071d22";
+static const char * const BAR_FG_COLOR = "#071d22";
 
 #endif
 #ifdef _IMAGE_CONFIG
